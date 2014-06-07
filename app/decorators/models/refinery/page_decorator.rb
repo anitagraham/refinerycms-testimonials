dc8cba2ae659019be207e504a01916d4067039b3
Anita Graham <anita@joli.com.au>
2014-06-07 21:52:00 +0800

    Testimonials getter into the Page model decorator
    Decorate the Page model instead of the page controller.
    Use attr_accessor to refer to page.testimonials

--- app/decorators/models/refinery/page_decorator.rb
+++ app/decorators/models/refinery/page_decorator.rb
@@ -1,9 +1,16 @@
 # Open the Refinery::Page model for manipulation
 
-Refinery::Page.class_eval do
-  has_many :testimonials
+@testRefinery::Page.class_eval do
   # Whitelist the testimonials control fields
   attr_accessor :testimonials
   attr_accessible :testimonials, :testimonials_show, :testimonials_count, :testimonials_select
   validates :testimonials_count, :numericality => { :only_integer => true, :greater_than_or_equal_to=>0 }
+
+  def testimonials
+    if testimonials_show
+      n = testimonials_count==0 ? Refinery::Testimonials::Testimonial.count : testimonials_count
+      @testimonials = Refinery::Testimonials::Testimonial.scoped
+      @testimonials = testimonials_select=='Random' ? random(n) : recent(n)
+    end
+  end
 end
