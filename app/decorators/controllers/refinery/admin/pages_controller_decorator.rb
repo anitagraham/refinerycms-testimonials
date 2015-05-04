1f9e41486d7f8a3a62080135006cba6545efe023
Anita Graham <anita@joli.com.au>
2015-05-04 14:13:37 +0800

    Revert to old decorator form
    Remove spec/dummy from repository

--- app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
@@ -1,7 +1,24 @@
-Refinery::Admin::PagesController.prepend(
-  Module.new do
-    def permitted_page_params
-      super <<  [:testimonials_show, :testimonials_count, :testimonials_select]
+#Refinery::Admin::PagesController.prepend(
+ # Module.new do
+  #  def permitted_page_params
+   #   super <<  [:testimonials_show, :testimonials_count, :testimonials_select]
+    #end
+#  end
+#)
+Refinery::Admin::PagesController.class_eval do
+
+  # work around an issue with stack level too deep, due to an issue with decorators.
+  if self.instance_methods.exclude?(:page_params_with_testimonial_params)
+    # We need to add testimonials attributes to page_params as it is ignored by strong parameters. (See #100)
+    def page_params_with_testimonial_params
+
+      # Get the :images_attributes hash from params
+      testimonial_params = params.require(:page).permit(:testimonials_show, :testimonials_count, :testimonials_select)
+      page_params_without_testimonial_params.merge(testimonial_params)
+
     end
+
+    # Swap out the default page_params method with our new one
+    alias_method_chain :page_params, :testimonial_params
   end
-)
+end
