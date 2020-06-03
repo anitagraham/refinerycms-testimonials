8cea83824f82f4d62c6d66c1784cb6c1741c45d1
Anita Graham <anita@joli.com.au>
2020-06-03 21:48:40 +0800

    validations causing a problem. remove for the short term

--- app/decorators/models/refinery/testimonials_page_decorator.rb
+++ app/decorators/models/refinery/testimonials_page_decorator.rb
@@ -1,13 +1,13 @@
 # Open the Refinery::Page model for manipulation
 Refinery::Page.class_eval do
 
-  valid_format_names = %w(full extract).freeze
-  valid_select_options = %w(recent random).freeze
+  # valid_format_names = %w(full extract).freeze
+  # valid_select_options = %w(recent random).freeze
 
-  validates :testimonials_format, inclusion: { in: valid_format_names,
-                                               message: "%{value} must be one of %{valid_format_names}" }
-  validates :testimonials_select, inclusion: { in: valid_select_options,
-                                               message: "%{value} must be one of %{valid_select_options}" }
+  # validates :testimonials_format, inclusion: { in: valid_format_names,
+  #                                              message: "%{value} must be one of %{valid_format_names}" }
+  # validates :testimonials_select, inclusion: { in: valid_select_options,
+  #                                              message: "%{value} must be one of %{valid_select_options}" }
   attr_reader :testimonials
 
   def testimonials
