37dab9550c6c24033e27bebc8622092d8430a671
Anita <anita@joli.com.au>
2023-02-16 20:10:25 +0800

    Removed distinct clause from Page.has_many_testimonials

--- app/decorators/models/refinery/page_testimonials_decorator.rb
+++ app/decorators/models/refinery/page_testimonials_decorator.rb
@@ -2,7 +2,7 @@ module Refinery
   module PageTestimonialsDecorator
     def self.prepended(base)
       base.has_many :appearances, class_name: '::Refinery::Testimonials::Appearance'
-      base.has_many :testimonials, -> { order(received_date: :desc).distinct },
+      base.has_many :testimonials, -> { order(received_date: :desc) },
                     through: :appearances,
                     class_name: "Refinery::Testimonials::Testimonial",
                     inverse_of: :pages
